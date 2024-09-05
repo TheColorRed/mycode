3a34e24d8124b1a935711b9250db013ee3f18bea
Naddy, Ryan <ryan_naddy@optum.com>
2024-09-05 10:55:34 -0500

    lab 20

--- next-api-integrations/pages/_app.js
+++ next-api-integrations/pages/_app.js
@@ -1,5 +1,15 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import { ApolloProvider } from "@apollo/client";
+import client from "../lib/apolloClient";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  // Wrap the entire application in the ApolloProvider
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
+
+export default MyApp;
